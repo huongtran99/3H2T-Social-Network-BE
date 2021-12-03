eec6094bc743e407574fe8b872780c4c9ab95eef
toannd1997@gmail.com <toannd1997@gmail.com>
2021-12-03 13:54:06 +0700

    change password

--- src/main/java/com/codegym/controller/UserRestController.java
+++ src/main/java/com/codegym/controller/UserRestController.java
@@ -1,12 +1,16 @@
 package com.codegym.controller;
 
+import com.codegym.model.dto.LoginForm;
+import com.codegym.model.dto.RegistrationForm;
 import com.codegym.model.entity.User;
 import com.codegym.service.user.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Pattern;
 import java.util.Optional;
 
 @RestController
@@ -15,6 +19,9 @@ import java.util.Optional;
 public class UserRestController {
     @Autowired
     private IUserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @GetMapping
     public ResponseEntity<Iterable<User>> showAll() {
@@ -28,4 +35,15 @@ public class UserRestController {
     }
 
 
+    @PutMapping("{id}")
+    public ResponseEntity<User> changePassWord(@PathVariable Long id, @RequestBody LoginForm passNew ) {
+
+        Optional<User> userOptional = userService.findById(id);
+        if (!userOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        userOptional.get().setPassword(passwordEncoder.encode(passNew.getPassword()));
+        userService.save(userOptional.get());
+        return new ResponseEntity<>(userOptional.get(), HttpStatus.OK);
+    }
 }
