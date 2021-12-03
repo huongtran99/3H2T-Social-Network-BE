fc9ed5380683bf17930eb62d64d4b5252540bfbc
truc-nguyenminh <truc.nguyenminh93@gmail.com>
2021-12-03 13:58:15 +0700

    save before merging

--- src/main/java/com/codegym/config/security/SecurityConfig.java
+++ src/main/java/com/codegym/config/security/SecurityConfig.java
@@ -102,7 +102,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().ignoringAntMatchers("/**");
         http.httpBasic().authenticationEntryPoint(restServicesEntryPoint());
         http.authorizeRequests()
-                .antMatchers("/", "/register", "/login").permitAll()
+                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
