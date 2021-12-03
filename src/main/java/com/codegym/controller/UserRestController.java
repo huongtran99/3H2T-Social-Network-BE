f4c1a2c1d8417eb7f7782ae2564f33ac7570e601
huong <mhuong1235@gmail.com>
2021-12-03 09:16:12 +0700

    merge dev

--- src/main/java/com/codegym/controller/UserRestController.java
+++ src/main/java/com/codegym/controller/UserRestController.java
@@ -1,12 +1,20 @@
 package com.codegym.controller;
 
+import com.codegym.model.dto.UserForm;
 import com.codegym.model.entity.User;
 import com.codegym.service.user.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
 
 @RestController
@@ -15,6 +23,9 @@ import java.util.Optional;
 public class UserRestController {
     @Autowired
     private IUserService userService;
+
+    @Value("${file-upload}")
+    private String fileUpload;
 
     @GetMapping
     public ResponseEntity<Iterable<User>> showAll() {
@@ -27,5 +38,44 @@ public class UserRestController {
         return userOptional.map(user -> new ResponseEntity<>(user, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @Validated @RequestBody UserForm userForm, BindingResult bindingResult) {
+        if (bindingResult.hasFieldErrors()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
 
+        Optional<User> userOptional = userService.findById(id);
+        if (!userOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        User user1 = new User();
+        user1.setId(id);
+        user1.setUsername(userOptional.get().getUsername());
+        user1.setPassword(userForm.getPassword());
+        user1.setEmail(userForm.getEmail());
+        user1.setPhone(userForm.getPhone());
+        user1.setBirthday(userForm.getBirthday());
+        user1.setGender(userForm.getGender());
+        user1.setStatus(userOptional.get().isStatus());
+        user1.setRoles(userOptional.get().getRoles());
+
+        MultipartFile multipartFile = userForm.getAvatar();
+        String fileName = multipartFile.getOriginalFilename();
+        try {
+            FileCopyUtils.copy(multipartFile.getBytes(), new File(fileUpload + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        MultipartFile multipartFile1 = userForm.getCover();
+        String fileName1 = multipartFile1.getOriginalFilename();
+        try {
+            FileCopyUtils.copy(multipartFile.getBytes(), new File(fileUpload + fileName1));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        user1.setAvatar(fileName);
+        user1.setCover(fileName1);
+        return new ResponseEntity<>(userService.save(user1), HttpStatus.OK);
+    }
 }
--- src/main/java/com/codegym/model/dto/UserForm.java
+++ src/main/java/com/codegym/model/dto/UserForm.java
@@ -1,23 +1,33 @@
 package com.codegym.model.dto;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Email;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Data
 public class UserForm {
     @NotEmpty
     private Long id;
 
+    @NotEmpty
     private String password;
 
+    @NotEmpty
+    @Email
     private String email;
 
+    @NotEmpty
+    @Pattern(regexp = "[0-9]{10}", message = "Phone length is 10 and about 0-9")
     private String phone;
 
+    @NotNull
     private String birthday;
 
+    @NotNull
     private String gender;
 
     private MultipartFile avatar;
