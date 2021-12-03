9fa8d0b942ce78f141285f4547a6acdedd9509b1
huongtran99 <72086373+huongtran99@users.noreply.github.com>
2021-12-03 13:55:44 +0700

    update jwtResponse

--- src/main/java/com/codegym/model/dto/JwtResponse.java
+++ src/main/java/com/codegym/model/dto/JwtResponse.java
@@ -9,11 +9,11 @@ public class JwtResponse {
     private String username;
     private String email;
     private Collection<? extends GrantedAuthority> roles;
-    private String token;
+    private String accessToken;
     private String type = "Bearer";
 
     public JwtResponse(String accessToken, Long id, String username, String email, Collection<? extends GrantedAuthority> roles) {
-        this.token = accessToken;
+        this.accessToken = accessToken;
         this.id = id;
         this.username = username;
         this.email = email;
@@ -49,11 +49,11 @@ public class JwtResponse {
     }
 
     public String getAccessToken() {
-        return token;
+        return accessToken;
     }
 
     public void setAccessToken(String accessToken) {
-        this.token = accessToken;
+        this.accessToken = accessToken;
     }
 
     public String getTokenType() {
