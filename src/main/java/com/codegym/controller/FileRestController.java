26e37f52d2d9113b378b05b7eaa385612cf91ca6
huongtran99 <72086373+huongtran99@users.noreply.github.com>
2021-12-08 16:35:36 +0700

    update editFile

--- src/main/java/com/codegym/controller/FileRestController.java
+++ src/main/java/com/codegym/controller/FileRestController.java
@@ -6,6 +6,7 @@ import com.codegym.model.entity.File;
 import com.codegym.model.entity.Post;
 import com.codegym.model.entity.User;
 import com.codegym.service.file.IFileService;
+import com.codegym.service.post.IPostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -25,12 +26,24 @@ public class FileRestController {
     @Autowired
     private IFileService fileService;
 
+    @Autowired
+    private IPostService postService;
+
     @Value("${file-upload}")
     private String fileUpload;
 
     @GetMapping
     public ResponseEntity<Iterable<File>> findAll() {
         return new ResponseEntity<>(fileService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/postId/{id}")
+    public ResponseEntity<Iterable<File>> getFileByPostId(@PathVariable Long id) {
+        Optional<Post> postOptional = postService.findById(id);
+        if (!postOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(fileService.findFileByPostId(id), HttpStatus.OK);
     }
 
     @PostMapping("/postId")
@@ -61,30 +74,34 @@ public class FileRestController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-//    @PutMapping("/{id}")
-//    public ResponseEntity<File> editFile(FileForm fileForm) {
-//        List<MultipartFile> multipartFiles = fileForm.getFileNames();
-//        for (MultipartFile file : multipartFiles) {
-//            String fileName = file.getOriginalFilename();
-//            if (fileName.equals("")) {
-//                fileName = String.valueOf(fileForm.getFileNames());
-//            } else {
-//                try {
-//                    FileCopyUtils.copy(file.getBytes(), new java.io.File(fileUpload + fileName));
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            File newFile = new File();
-//            newFile.setId(fileForm.getId());
-//            newFile.setFileName(fileName);
-//            newFile.setPost(fileForm.getPost());
-//            newFile.setComment(fileForm.getComment());
-//            newFile.setMessage(fileForm.getMessage());
-//            fileService.save(newFile);
-//        }
-//        return new ResponseEntity<>(HttpStatus.OK);
-//    }
+    @PutMapping("/{id}")
+    public ResponseEntity<File> editFile(@PathVariable Long id, FileForm fileForm) {
+        Optional<File> fileOptional = fileService.findById(id);
+        if (!fileOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        List<MultipartFile> multipartFiles = fileForm.getFileNames();
+        for (MultipartFile file : multipartFiles) {
+            String fileName = file.getOriginalFilename();
+            if (fileName.equals("")) {
+                fileName = String.valueOf(fileForm.getFileNames());
+            } else {
+                try {
+                    FileCopyUtils.copy(file.getBytes(), new java.io.File(fileUpload + fileName));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            File newFile = new File();
+            newFile.setId(fileOptional.get().getId());
+            newFile.setFileName(fileName);
+            newFile.setPost(fileForm.getPost());
+            newFile.setComment(fileForm.getComment());
+            newFile.setMessage(fileForm.getMessage());
+            fileService.save(newFile);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<File> deleteFileById(@PathVariable Long id) {
